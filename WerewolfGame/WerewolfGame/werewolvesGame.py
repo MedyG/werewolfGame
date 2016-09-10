eb554b43ad28c195cba0e582233386615058db85
MedyG <guohx3@hotmail.com>
2016-09-10 12:22:08 +0800

    加入html模板示例

--- WerewolfGame/WerewolfGame/werewolvesGame.py
+++ WerewolfGame/WerewolfGame/werewolvesGame.py
@@ -3,12 +3,21 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        #self.write("Hello, world")
+        self.render("index.html", list_info = {11, 22, 33})
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-    ])
+    ], **settings)
+
+''' settings
+'''
+settings = {
+    "template_path" : "views",            # html文件
+    "static_path": "static",              # 静态文件路径（css，js，img）
+    "static_url_prefix" : "/static/",     # 静态文件前缀
+}
 
 if __name__ == "__main__":
     app = make_app()
