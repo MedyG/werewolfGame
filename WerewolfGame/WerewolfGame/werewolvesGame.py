af2d080eaace45a13f54d7902595b3f2596f4265
MedyG <guohx3@hotmail.com>
2016-09-11 00:33:40 +0800

    bug fixed

--- WerewolfGame/WerewolfGame/werewolvesGame.py
+++ WerewolfGame/WerewolfGame/werewolvesGame.py
@@ -4,7 +4,7 @@ import tornado.web
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         #self.write("Hello, world")
-        self.render("index.html", list_info = {11, 22, 33})
+        self.render("index.html", list_info = [11, 22, 33])
 
 def make_app():
     return tornado.web.Application([
